c53f5d49e87c58e120f3c4d34137a9d70b6b4eba
Jarvist Moore Frost <jarvist@gmail.com>
2017-04-13 12:03:13 +0100

    Restructure into function; make callable.

--- FeynmanKadanoffOsakaHellwarth.jl
+++ FeynmanKadanoffOsakaHellwarth.jl
@@ -4,6 +4,16 @@
 # If you run this, it should construct the model, solve for varying temperature, then produce plots as .pngs in the local directory.
 # These codes were developed with Julia 0.5.0, and requires the Optim and Plots packages.
 
+##### load in library routines... #####
+# one-dimensional numerical integration in Julia using adaptive Gauss-Kronrod quadrature
+using QuadGK
+# Plot figures with Plots, which defaults to Pyplot backend
+using Plots
+default(size=(800,600)) # For the .png file output
+# Using the powerful Julia Optim package to optimise the variational parameters
+using Optim
+
+
 # Physical constants
 const hbar = const ħ = 1.05457162825e-34;          # kg m2 / s 
 const eV = const q = const ElectronVolt = 1.602176487e-19;                         # kg m2 / s2 
@@ -47,42 +57,11 @@ end
 #checkalpha()
 
 #####
-# Set up the simulation parameters
-
-# Internally we have 'mb' for the 'band mass' in SI units, of the effecitve-mass of the electron
-effectivemass=0.12 # the bare-electron band effective-mass. 
-# --> 0.12 for electrons and 0.15 for holes, in MAPI. See 2014 PRB.
-mb=effectivemass*MassElectron 
-
-# MAPI  4.5, 24.1, 2.25THz - 75 cm^-1 ; α=
-ε_Inf=4.5
-ε_S=24.1
-freq=2.25E12 # 2.25 THz
-
-# PCBM: 4.0, 5.0, 2.25Thz, effective-mass=1.0
-# CsPbI3: 3.82, 8.27, 2.83 THz (highest freq mode in cubic) 
-#    ^-- v. quick Kmesh=3x3x3 PBESol Calc, JMF 2017-04-12. Files: jarvist@titanium:~/phonopy-work/2017-03-Scott-Distortions/1000-Dielectric/3x3x3-kmesh/
-ε_Inf=3.82
-ε_S=8.27
-freq=2.83E12 # 2.25 THz
-
-ω = (2*pi)*freq # angular-frequency
-
-α=feynmanalpha(ε_Inf, ε_S,  freq,    effectivemass)
-#α=2.395939683378253 # Hard coded; from MAPI params, 4.5, 24.1, 2.25THz, 0.12me
-
-@printf("Polaron mobility input parameters: ε_Inf=%f ε_S=%f freq=%g α=%f \n",ε_Inf, ε_S, freq, α )
-@printf("Derived params in SI: ω =%g mb=%g \n",ω ,mb)
-
-#####
 # Set up equations for the polaron free energy, which we will variationally improve upon
 
 # Hellwarth 1999 PRB - Part IV; T-dep of the Feynman variation parameter
 # Originally a Friday afternoon of hacking to try and implement the T-dep electron-phonon coupling from the above PRB
 # Which was unusually successful! And more or less reproduced Table III
-
-# one-dimensional numerical integration in Julia using adaptive Gauss-Kronrod quadrature
-using QuadGK
 
 # Define Osaka's free-energies (Hellwarth1999 version) as Julia functions
 # Equation numbers follow above Hellwarth 1999 PRB
@@ -105,8 +84,19 @@ F(v,w,β,α)=-(A(v,w,β)+B(v,w,β,α)+C(v,w,β)) #(62a)
 # Also there's a scary numeric integration (quadgk) buried within...
 
 #####
-# OK, let's use the powerful Julia Optim package to optimise the variational parameters
-using Optim
+# OK, this was all in the global scope, but has now been put within a function so it can be called for varying parameters
+function polaronmobility(fileprefix,ε_Inf, ε_S,  freq,    effectivemass)
+@printf("Calculating polaron mobility for %s ...\n",fileprefix)
+
+# Internally we have 'mb' for the 'band mass' in SI units, of the effecitve-mass of the electron
+mb=effectivemass*MassElectron 
+ω = (2*pi)*freq # angular-frequency
+    
+α=feynmanalpha(ε_Inf, ε_S,  freq,    effectivemass)
+#α=2.395939683378253 # Hard coded; from MAPI params, 4.5, 24.1, 2.25THz, 0.12me
+
+@printf("Polaron mobility input parameters: ε_Inf=%f ε_S=%f freq=%g α=%f \n",ε_Inf, ε_S, freq, α )
+@printf("Derived params in SI: ω =%g mb=%g \n",ω ,mb)
 
 # Initial v,w to use
 initial=[7.1,6.5]
@@ -249,38 +239,35 @@ for T in 10:10:400
     initial=[v,w] # Caution! Might cause weird sticking in local minima
 end
 
-println("OK - everything calculated and stored. Now plotting figures..")
-
-using Plots
-default(size=(800,600))
+println("OK - everything calculated and stored. Now plotting..")
 
 #####
 ## Mass vs. Temperature plot
 plot(Ts,Ms,label="Phonon effective-mass",marker=2,xlab="Temperature (K)",ylab="Phonon effective-mass (units bare-electron effective-masses)",ylim=(0,1.2))
 
-savefig("mass.png")
-savefig("mass.eps")
+savefig("$fileprefix-mass.png")
+savefig("$fileprefix-mass.eps")
 
 #####
 ## Relaxationtime vs. Temperature plot
 plot(Ts,Taus,label="Boltzmann eqn. relaxation time (ps)",marker=2,xlab="Temperature (K)",ylab="Relaxation time (ps)",ylim=(0,1.2))
 
-savefig("tau.png")
-savefig("tau.eps")
+savefig("$fileprefix-tau.png")
+savefig("$fileprefix-tau.eps")
 
 ## Mass + relaxation time vs. Temperature plot
 plot(Ts,Ms,label="Phonon effective-mass",marker=2,xlab="Temperature (K)",ylab="Phonon effective-mass (units bare-electron effective-masses)",ylim=(0,1.2))
 plot!(Ts,Taus,label="Boltzmann eqn. relaxation time (ps)",marker=2,xlab="Temperature (K)",ylab="Relaxation time (ps)",ylim=(0,1.2))
 
-savefig("mass-tau.png")
-savefig("mass-tau.eps")
+savefig("$fileprefix-mass-tau.png")
+savefig("$fileprefix-mass-tau.eps")
 
 #####
 ## Spring Constants vs. Temperature plot
 plot(Ts,ks,label="Polaron spring-constant",marker=2, xlab="Temperature (K)",ylab="Spring-const (some internal unit)",)
 
-savefig("spring.png")
-savefig("spring.eps")
+savefig("$fileprefix-spring.png")
+savefig("$fileprefix-spring.eps")
 
 #####
 ## Variation Energy vs. Temperature plots
@@ -289,8 +276,8 @@ plot!(Ts,Bs,label="B",marker=2)
 plot!(Ts,Cs,label="C",marker=2)
 plot!(Ts,Fs,label="F",marker=2)
 
-savefig("variational.png")
-savefig("variational.eps")
+savefig("$fileprefix-variational.png")
+savefig("$fileprefix-variational.eps")
 
 #####
 ## Calculated mobility comparison plot
@@ -298,8 +285,33 @@ plot(Ts,Kμs,label="Kadanoff",marker=2,xlab="Temperature (K)",ylab="Mobility (cm
 plot!(Ts,FHIPμs,label="FHIP",marker=2)
 plot!(Ts,Hμs,label="Hellwarth1999",marker=2)
 
-savefig("mobility-calculated.png")
-savefig("mobility-calculated.eps")
+savefig("$fileprefix-mobility-calculated.png")
+savefig("$fileprefix-mobility-calculated.eps")
+
+
+return(Ts,Kμs, Hμs, FHIPμs, ks, Ms, As, Bs, Cs, Fs, Taus)
+end
+
+#####
+# Call simulation
+
+#Ts,Kμs, Hμs, FHIPμs, ks, Ms, As, Bs, Cs, Fs, Taus
+#effectivemass=0.12 # the bare-electron band effective-mass. 
+# --> 0.12 for electrons and 0.15 for holes, in MAPI. See 2014 PRB.
+# MAPI  4.5, 24.1, 2.25THz - 75 cm^-1 ; α=
+Ts,eKμs, eHμs, eFHIPμs, eks, eMs, eAs, eBs, eCs, eFs, eTaus = polaronmobility("MAPI-electron", 4.5, 24.1, 2.25E12, 0.12)
+Ts,hKμs, hHμs, hFHIPμs, hks, hMs, hAs, hBs, hCs, hFs, hTaus = polaronmobility("MAPI-hole",     4.5, 24.1, 2.25E12, 0.15)
+
+# PCBM: 4.0, 5.0, 2.25Thz, effective-mass=1.0
+#polaronmobility("PCBM",     4.0, 5.0, 2.25E12, 1.00)
+
+# CsPbI3: 3.82, 8.27, 2.83 THz (highest freq mode in cubic) 
+#    ^-- v. quick Kmesh=3x3x3 PBESol Calc, JMF 2017-04-12. Files: jarvist@titanium:~/phonopy-work/2017-03-Scott-Distortions/1000-Dielectric/3x3x3-kmesh/
+# Horribly non-converged! Kmesh=6x6x6; 7.2/12.1
+# Kmesh=9x9x9x, Ediff=10^-9;           6.1/12.0, 2.57 THz
+#polaronmobility("CsPbI3-electron",     6.1,12.0, 2.57E12, 0.12)
+
+
 
 #####
 ## Expt. data to compare against
@@ -343,11 +355,17 @@ xlab="Temperature (K)",ylab="Mobility (cm^2/Vs)",marker=2, ylims=(0,400) )
 plot!(Saidaminov[:,1],Saidaminov[:,2],label="Saidaminov JV Single Crystal", marker=6)
 plot!(Semonin[:,1],Semonin[:,2],label="Semonin Single Crystal TRMC", marker=6)
 
-plot!(Ts,Kμs,label="Kadanoff Polaron mobility",marker=2)
-plot!(Ts,Hμs,label="Hellwarth1999 Polaron mobility",marker=2)
+#plot!(Ts,eKμs,label="(electrons) Kadanoff Polaron mobility",marker=2)
+plot!(Ts,eHμs,label="(electrons) Hellwarth1999 Polaron mobility",marker=2)
 
-savefig("mobility-calculated-experimental.png")
-savefig("mobility-calculated-experimental.eps")
+#plot!(Ts,hKμs,label="(holes) Kadanoff Polaron mobility",marker=2)
+plot!(Ts,hHμs,label="(holes) Hellwarth1999 Polaron mobility",marker=2)
+
+savefig("MAPI-eh-mobility-calculated-experimental.png")
+savefig("MAPI-eh-mobility-calculated-experimental.eps")
 
 
 println("That's me!")
+
+
+
