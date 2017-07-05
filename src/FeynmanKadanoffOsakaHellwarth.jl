cc451fa6a41f062252ba619db06f08d4260a0355
Jarvist Moore Frost <jarvist@gmail.com>
2017-07-05 22:58:29 +0100

    More info on numerical integration breakdown. Roundoff -> 0 error?

--- src/FeynmanKadanoffOsakaHellwarth.jl
+++ src/FeynmanKadanoffOsakaHellwarth.jl
@@ -376,16 +376,23 @@ function polaronmobility(fileprefix,Trange, ε_Inf, ε_S,  freq,    effectivemas
         # Hellwarth1999 limit of v->0
         @printf("Ain't no Imaginary Impedance like a Feynman 1962 ImX...")
         # Feynman, I love you - but using Nu, v; Omega, w in the same paper + formulas, for similar objects?!
-        for nu=0:0.1:20
+        for nu=0:0.5:20
             R=(v^2-w^2)/(w^2*v) # inline, page 300 just after Eqn (2)
             b=R*βred/sinh(βred*v/2) # Feynman1962 version; page 1010, Eqn (47b)
             a=sqrt( (βred/2)^2 + R*βred*coth(βred*v/2))
             k(u,a,b,v,nu) = (u^2+a^2-b*cos(v*u))^(-3/2)*cos(u)*cos(nu*u) # integrand with cos(vu) term, as (47a) 
-            K=quadgk(u->k(u,a,b,v,nu),0,Inf)[1] # numerical quadrature integration of (2)
+            @printf("Numerical integration of Feynman1962(42a): nu=%.2f ",nu)
+            @time n=quadgk(u->k(u,a,b,v,nu),0,Inf,maxevals=10^9,reltol=0.1) # numerical quadrature integration of (2)
+            K=n[1]
+            err=n[2]
+            @printf(" quadgk: K=%g err=%g\n",K,err)
+
             # Full 47a constructed here
             ImX= 2*α/(3*sqrt(π)) * βred^(3/2) * (sinh(βred*nu/2))/sinh(βred/2) * (v^3/w^3) * K
 
-            @printf(" %.3f %g\n",nu,ImX)
+            μ=ImX^-1 * (q)/(ω*mb)
+ 
+            @printf(" %.3f %g %g\n",nu,ImX,μ)
         end
 
         @printf("\n\n")
