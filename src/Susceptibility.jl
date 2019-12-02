fc6e175e72f167dfa7b15a32270d2f6901d4852c
Jarvist Moore Frost <jarvist@gmail.com>
2019-12-02 12:06:39 +0000

    Added Fourier-integral style quadrature to improve numeric stability
    Still not that happy with this.
    Took the form from the GSL page on this problem, after playing around
    with directly using the GSL via GSL.jl. (Not much point doing this, as
    even though it was super-quick, round-off errors were a bit difficult to
    predict.)
    I do not implement the epsilon-acceleration, just the calculation of the
    bounds for the quadrature, and then hope that QuadGK.jl sorts out
    everything else.
    
    Horribly oscillatory!

--- src/Susceptibility.jl
+++ src/Susceptibility.jl
@@ -35,11 +35,33 @@ function ImX(nurange,v,w,βred,α,ω,mb)
 
         @printf("Numerical integration of FHIP1962(42a): nu=%.2f ",nu)
         println("   a: $(a) b:$(b) ")
-        @time n=quadgk(u->k(u,a,b,v,nu),0,Inf,maxevals=10^6,rtol=0.001, order=9) # numerical quadrature integration of (2)
+       
+        # This is a simple approximation of part of the
+        # https://www.gnu.org/software/gsl/doc/html/integration.html#qawf-adaptive-integration-for-fourier-integrals
+        # GSL adaptive method for Fourier integrals
+        # We split the integral into a load of integrals, balanced at some of
+        # the roots.
+        c=(2*floor(nu)+1)*π/nu
+        println("Fourier integral c: $(c)")
+        fourier_range = [ c*i for i in 0:2501 ] 
+        
+        # Catch Inf c range
+        if nu==0
+            fourier_range = [0, Inf]
+        end
+
+        # These params tweaked to get the best behaviour at reproducing
+        # Mishchenko-Fig4 in a timely manner.
+        @time n=quadgk(u->k(u,a,b,v,nu),fourier_range... , 
+                       maxevals=10^6,rtol=0.0001, atol=1e-15, order=7) # numerical quadrature integration of (2)
         K=n[1]
         err=n[2]
         @printf(" quadgk: K=%g err=%g\n",K,err)
         
+        if K<err # we've lost control of our errors, due to losing the oscillatory war with nu
+            break # --> so give up.
+        end
+
         # Full 47a constructed here
         ImX= 2*α/(3*sqrt(π)) * βred^(3/2) * (sinh(βred*nu/2))/sinh(βred/2) * (v^3/w^3) * K
 
