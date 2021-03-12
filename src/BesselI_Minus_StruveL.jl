7ca5dccbd6417913cf4f7c6c0191810dec6bc261
Brad <bam116@ic.ac.uk>
2021-03-12 14:34:27 +0000

    Corrected ArbReal prec (prec + 8) compared to other float types. Correctly parsed arguments into ArbReal type.

--- src/BesselI_Minus_StruveL.jl
+++ src/BesselI_Minus_StruveL.jl
@@ -7,10 +7,13 @@ function BesselI_minus_StruveL(n, z; prec = 64) # z > 0 & n >= 0
 
     # Initialise precision of ArbReal to prec.
     setextrabits(0)
-    setprecision(ArbReal, prec)
+    setprecision(ArbReal, prec + 8)
 
-    k = ArbReal(0)
-    result = ArbReal(0.0)
+    n = ArbReal("$n")
+    z = ArbReal("$z")
+
+    k = ArbReal("0")
+    result = ArbReal("0.0")
     err = eps(result)  # Machine accuracy of specified precision prec.
 
     while true
@@ -19,25 +22,31 @@ function BesselI_minus_StruveL(n, z; prec = 64) # z > 0 & n >= 0
         term = bessel_term - struve_term
 
         # Break loop if term smaller than accuracy of result.
-        if abs(term) < eps(result)
+        if abs(term) < eps(result)^2
             break
         end
 
         result += term
-        k += ArbReal(1)
+        k += ArbReal("1")
 
         # Double precision if rounding error in result exceeds accuracy specified by prec.
         if ball(result)[2] > err
             setprecision(ArbReal, precision(result) * 2)
-            k = ArbReal(0)
-            result = ArbReal(0.0)
+            n = ArbReal("$n")
+            z = ArbReal("$z")
+            k = ArbReal("0")
+            result = ArbReal("0.0")
         end
     end
-    ArbReal(result, bits = prec)
+    ArbReal(result, bits = prec + 8)
 end
 
-x = 0.001:1:200
-@time S = abs.(BesselI_minus_StruveL.(1, x; prec = 512))
+x = 1:200
+@time S = abs.(BesselI_minus_StruveL.(1, x; prec = 256))
 p = plot(x, S, yaxis=:log)
 display(p)
 @show(S)
+
+# @time S_test = BesselI_minus_StruveL(1, 1/2; prec = 512)
+# t_test = parse(BigFloat, "-0.4326676496002076900053886410785092156777939805674897110150085171427097841422348277548777662847685061084103473774743461230956021691253453509671770438054326932177452541623496380111820545155461149382516536713540154883355853639545855374918858504158554993")
+# @show(S_test, t_test)
