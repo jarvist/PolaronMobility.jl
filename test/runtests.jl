78358c640d7eb0bf73e99e19b4190536f576021a
Jarvist Moore Frost <jarvist@gmail.com>
2021-06-09 14:59:42 +0100

    Bringing back Oct2019 Multiple branch tests; rename Hellwarth Stuff

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,9 +13,9 @@ end
 
 include("FrohlichAlpha.jl") # Simple test of Frohlich alpha vs. literature values
 include("FeynmanAthermal.jl") # Athermal Feynman tests
-include("HellwarthMultipleBranches.jl") # Test Hellwarth et al. 1999 PRB 'B' multiple branch reduction scheme
+include("HellwarthEffectiveFrequency.jl") # Test Hellwarth et al. 1999 PRB 'B' multiple branch reduction scheme
 include("FrostPolaronMobility2017.jl") # Reproduce values published in Frost 2017 PRB 
-#include("MultipleBranches.jl") # Test explicit Oct 2019:-> explicit phonon branches
+include("MultipleBranches.jl") # Test explicit Oct 2019:-> explicit phonon branches
 end
 
 println("\nThat's me! If I finished without interupting, all tests have passed.")
