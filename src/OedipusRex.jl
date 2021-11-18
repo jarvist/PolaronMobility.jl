633105f395085da626031f99e36595354edb367a
Jarvist Moore Frost <jarvist@gmail.com>
2021-11-18 23:37:23 +0000

    Formatting & full reference.

--- src/OedipusRex.jl
+++ src/OedipusRex.jl
@@ -189,7 +189,15 @@ Polaron absorption coefficient Γ(Ω).
 """
 optical_absorption(Ω::Float64, α::Float64, v::Float64, w::Float64)
 
-    Calculate the absorption coefficient Γ(Ω) for the polaron at at finite temperatures (equation (11a) in Devreese's et al.) for a given frequency Ω. v and w are the variational Polaron parameters that minimise the free energy, for the supplied α Frohlich coupling.
+    Calculate the absorption coefficient Γ(Ω) for the polaron at at finite temperatures
+    (equation (11a) in [1]) for a given frequency Ω. v and w are the
+    variational Polaron parameters that minimise the free energy, for the supplied
+    α Frohlich coupling.  
+
+    [1] Devreese, J., De Sitter, J., & Goovaerts, M. (1972). Optical Absorption of Polarons
+    in the Feynman-Hellwarth-Iddings-Platzman Approximation. Physical Review B, 5(6),
+    2367–2381.  doi:10.1103/physrevb.5.2367 
+
 """
 function optical_absorption(Ω, β, α, v, w)
     X = χ(Ω, β, α, v, w)[1]
@@ -202,7 +210,15 @@ end
 """
 optical_absorption(Ω::Float64, β::Float64, α::Float64, v::Float64, w::Float64)
 
-    Calculate the absorption coefficient Γ(Ω) for the polaron at finite temperatures (equation (11b) in Devreese's et al.) for a given frequency Ω. β is the thermodynamic beta. v and w are the variational Polaron parameters that minimise the free energy, for the supplied α Frohlich coupling.
+    Calculate the absorption coefficient Γ(Ω) for the polaron at finite temperatures
+    (equation (11b) in [1]) for a given frequency Ω. β is the thermodynamic
+    beta. v and w are the variational Polaron parameters that minimise the free energy, for
+    the supplied α Frohlich coupling.
+
+    [1] Devreese, J., De Sitter, J., & Goovaerts, M. (1972). Optical Absorption of Polarons
+    in the Feynman-Hellwarth-Iddings-Platzman Approximation. Physical Review B, 5(6),
+    2367–2381.  doi:10.1103/physrevb.5.2367 
+
 """
 # function optical_absorption(Ω, β, α, v, w)
 #     ((Ω^2 - w^2)^2 / (Ω^5 - Ω^3 * v^2)^2) * imag(χ(Ω, β, α, v, w)[1])
