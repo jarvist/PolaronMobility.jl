e0e36ce61f8e3ff5f570d7f9f5c90a8c48df1ba5
Bradley Martin <bam116@ic.ac.uk>
2021-12-02 19:15:27 +0000

    Update MultipleBranches.jl
    Add comments and explanations of functions. Still wip.

--- src/MultipleBranches.jl
+++ src/MultipleBranches.jl
@@ -111,43 +111,143 @@ function Hellwarth1999mobilityRHS( (α, (v,w) ,f) , effectivemass, T)
     return 1/μ
 end
 
-"Multiple branches frohlich α"
+"""
+----------------------------------------------------------------------
+Multiple Branch Frohlich Alpha
+----------------------------------------------------------------------
+
+This section of the code is dedicated to determining the partial dielectric electron-phonon coupling parameter, decomposed into ionic dielectric contributions from each phonon mode of the matieral. 
+"""
+
+"""
+ϵ_ionic_mode(phonon_mode_freq::Float64, ir_activity::Float64, volume::Float64)
+
+    Calculate the ionic contribution to the dielectric function for a given phonon mode.
+    phonon_mode_freq is the frequency of the mode in THz.
+
+     - ir_activity is the infra-red activity of the mode in e^2 amu^-1.
+     - volume is the volume of the unit cell of the material in m^3.
+"""
 function ϵ_ionic_mode(phonon_mode_freq, ir_activity, volume) # single ionic mode
-    ω_j = 2π * phonon_mode_freq * 1e12 # angular phonon freq in Hz
-    ϵ_mode = eV^2 * ir_activity / (3 * volume * ω_j^2 * amu) # single ionic mode
-    return ϵ_mode / ϵ_0 # normalise with 1 / (4π ϵ_0)
+
+    # Angular phonon frequency for the phonon mode (rad Hz)
+    ω_j = 2π * phonon_mode_freq * 1e12 
+
+    # Dielectric contribution from a single ionic phonon mode
+    ϵ_mode = eV^2 * ir_activity / (3 * volume * ω_j^2 * amu)
+
+    # Normalise ionic dielectric contribution with 1 / (4π ϵ_0) (NB: the 4π has been pre-cancelled)
+    return ϵ_mode / ϵ_0 
 end
 
+"""
+ϵ_total(freqs_and_ir_activity::Matrix{Float64}, volume::Float64)
+
+    Calculate the total ionic contribution to the dielectric function from all phonon modes.
+
+     - freqs_and_ir_activity is a matrix containing the phonon mode frequencies (in THz) in the first column and the infra-red activities (in e^2 amu^-1) in the second column.
+     - volume is the volume of the unit cell of the material in m^3.
+"""
 function ϵ_total(freqs_and_ir_activity, volume) # total ionic contribution to dielectric
+
+    # Extract phonon frequencies (THz)
     phonon_freqs = freqs_and_ir_activity[:, 1] 
+
+    # Extra infra-red activities (e^2 amu^-1)
     ir_activity = freqs_and_ir_activity[:, 2]
-    result = 0.0
+
+    # Sum over all ionic contribution from each phonon mode
+    total_ionic = 0.0
     for (f, r) in zip(phonon_freqs, ir_activity)
-        result += ϵ_ionic_mode(f, r, volume) # sum over all ionic contributions
+        total_ionic += ϵ_ionic_mode(f, r, volume) 
     end
-    return result
+
+    return total_ionic
 end
 
-function effective_freqs(freqs_and_ir_activity, num_var_params) #PCA Algorithm
-    standardized_matrix = freqs_and_ir_activity' .- mean(freqs_and_ir_activity', dims = 2) # centralise data by subtracting columnwise mean
-    covariance_matrix = standardized_matrix' * standardized_matrix # has 1 / (n - 1) for number of params n = 2
-    eigenvectors = eigvecs(covariance_matrix) # eigenvectors to project data along
-    reduced_matrix = # project data along eigenvectors and undo centralisation
-    standardized_matrix[:, 1:num_var_params] * eigenvectors[1:num_var_params, 1:num_var_params] * 
-    eigenvectors[1:num_var_params, 1:num_var_params]' .+ mean(freqs_and_ir_activity', dims = 2)
-    return abs.(reduced_matrix')
+"""
+effective_freqs(freqs_and_ir_activity::Matrix{Float64}, num_var_params::Integer)
+
+    Generates a matrix of effective phonon modes with frequencies and infra-red activities derived from a larger matrix using the Principal Component Analysis (PCA) method.
+
+     - freqs_and_ir_activity: is a matrix containing the phonon mode frequencies (in THz) in the first column and the infra-red activities (in e^2 amu^-1) in the second column.
+     - num_var_params: is the number of effective modes required (which needs to be less than the number of modes in freqs_and_ir_activity)
+
+    *** POSSIBLY REDUNDANT ***
+"""
+function effective_freqs(freqs_and_ir_activity, num_var_params) # PCA Algorithm
+
+    # Check that the number of effective modes is less than the number of actual phonon modes.
+    if num_var_params >= size(freqs_and_ir_activity)[1]
+
+        println("The number of effective phonon modes has to be less than the total number of phonon modes.")
+
+    else
+
+        # Centralise data by subtracting the columnwise mean
+        standardized_matrix = freqs_and_ir_activity' .- mean(freqs_and_ir_activity', dims = 2) 
+
+        # Calculate the covariance matrix S' * S. Matrix size is (n - 1) x (n - 1) for number of params (here n = 2)
+        covariance_matrix = standardized_matrix' * standardized_matrix 
+
+        # Extract eigenvectors of the covariance matrix
+        eigenvectors = eigvecs(covariance_matrix) 
+
+        # Project the original data along the covariance matrix eigenvectors and undo the centralisation
+        reduced_matrix = standardized_matrix[:, 1:num_var_params] * eigenvectors[1:num_var_params, 1:num_var_params] * 
+        eigenvectors[1:num_var_params, 1:num_var_params]' .+ mean(freqs_and_ir_activity', dims = 2)
+
+        # Resultant matrix is positive definite and transposed.
+        return abs.(reduced_matrix')
+    end
 end
 
-function frohlich_α_j(ϵ_optic, ϵ_ionic, ϵ_total, phonon_mode_freq, m_eff) # Frohlich alpha decomposed into phonon branch contributions
-    Ry = eV^4 * me / (2 * ħ^2) # Rydberg energy
-    ω = 2π * 1e12 * phonon_mode_freq # angular phonon freq (Hz)
-    ϵ_static = ϵ_total + ϵ_optic # static dielectric. Calculate here instead of input so that ionic modes properly normalised.
-    return (m_eff * Ry / (ħ * ω))^(1 / 2) * ϵ_ionic / (4π * ϵ_0) / (ϵ_optic * ϵ_static) # 1 / (4π ϵ_0) dielectric normalisation
+"""
+frohlich_α_j(ϵ_optic::Float64, ϵ_ionic::Float64, ϵ_total::Float64, phonon_mode_freq::Float64, m_eff::Float64)
+
+    Calculates the partial dielectric electron-phonon coupling parameter for a given longitudinal optical phonon mode. This decomposes the original Frohlich alpha coupling parameter (defined for a single phonon branch) into contributions from multiple phonon branches.
+
+     - ϵ_optic is the optical dielectric constant of the material.
+     - ϵ_ionic is the ionic dielectric contribution from the phonon mode.
+     - ϵ_total is the total ionic dielectric contribution from all phonon modes of the material.
+     - phonon_mode_freq is the frequency of the phonon mode (THz).
+     - m_eff is the band mass of the electron (in units of electron mass m_e)
+"""
+function frohlich_α_j(ϵ_optic, ϵ_ionic, ϵ_total, phonon_mode_freq, m_eff) 
+
+    # The Rydberg energy unit
+    Ry = eV^4 * me / (2 * ħ^2)
+
+    # Angular phonon frequency for the phonon mode (rad Hz)
+    ω = 2π * 1e12 * phonon_mode_freq 
+
+    # The static dielectric constant. Calculated here instead of inputted so that ionic modes are properly normalised.
+    ϵ_static = ϵ_total + ϵ_optic
+
+    # The contribution to the electron-phonon parameter from the currrent phonon mode. 1 / (4π ϵ_0) is the dielectric normalisation.
+    α_j  = (m_eff * Ry / (ħ * ω))^(1 / 2) * ϵ_ionic / (4π * ϵ_0) / (ϵ_optic * ϵ_static)
+
+    return α_j
 end
 
-"Multiple Phonon Branches"
+"""
+----------------------------------------------------------------------
+Multiple Branch Polaron Free Energy
+----------------------------------------------------------------------
 
-function κ_i(i, v, w) # fictitious spring constant, multiple variational params
+This section of the code is dedicated to calculating the polaron free energy, generalised from Osaka's expression to the case where multiple phonon modes are present in the material.
+"""
+
+"""
+κ_i(i::Integer, v::Array{Float64}(undef, 1), w::Array{Float64}(undef, 1))
+
+    Calculates the spring-constant coupling the electron to the `ith' fictitious mass that approximates the exact electron-phonon interaction with a harmonic coupling to a massive fictitious particle. NB: Not to be confused with the number of physical phonon branches; many phonon branches could be approximated with one or two etc. fictitious masses for example. The number of fictitious mass does not necessarily need to match the number of phonon branches.
+
+     - i enumerates the current fictitious mass.
+     - v is an one-dimensional array of the v variational parameters.
+     - w is an one-dimensional array of the w variational parameters.
+"""
+function κ_i(i, v, w)
     κ = v[i]^2 - w[i]^2
     if length(v) > 1
         for j in 1:length(v)
@@ -159,7 +259,16 @@ function κ_i(i, v, w) # fictitious spring constant, multiple variational params
     return κ
 end
 
-function h_i(i, v, w) # some vector relating to harmonic eigenmodes
+"""
+h_i(i::Integer, v::Array{Float64}(undef, 1), w::Array{Float64}(undef, 1))
+
+    Calculates the normal-mode (the eigenmodes) frequency of the coupling between the electron and the `ith' fictitious mass that approximates the exact electron-phonon interaction with a harmonic coupling to a massive fictitious particle. NB: Not to be confused with the number of physical phonon branches; many phonon branches could be approximated with one or two etc. fictitious masses for example. The number of fictitious mass does not necessarily need to match the number of phonon branches.
+
+     - i enumerates the current fictitious mass.
+     - v is an one-dimensional array of the v variational parameters.
+     - w is an one-dimensional array of the w variational parameters.
+"""
+function h_i(i, v, w)
     h = v[i]^2 - w[i]^2
     if length(v) > 1
         for j in 1:length(v)
@@ -171,7 +280,16 @@ function h_i(i, v, w) # some vector relating to harmonic eigenmodes
     return h
 end
 
-function C_ij(i, j, v, w) # generalised Feynman C variational parameter (inclusive of multiple v and w params)
+"""
+C_ij(i::Integer, j::Integer, v::Array{Float64}(undef, 1), w::Array{Float64}(undef, 1))
+
+    Calculates the element to the coupling matrix C_ij (a generalisation of Feynman's `C` coupling variational parameter) between the electron and the `ith' and `jth' fictitious masses that approximates the exact electron-phonon interaction with a harmonic coupling to a massive fictitious particle. NB: Not to be confused with the number of physical phonon branches; many phonon branches could be approximated with one or two etc. fictitious masses for example. The number of fictitious mass does not necessarily need to match the number of phonon branches.
+
+     - i, j enumerate the current fictitious masses under focus (also the index of the element in the coupling matrix C)
+     - v is an one-dimensional array of the v variational parameters.
+     - w is an one-dimensional array of the w variational parameters.
+"""
+function C_ij(i, j, v, w)
     C = w[i] * κ_i(i, v, w) * h_i(j, v, w) / (4 * (v[j]^2 - w[i]^2))
     return C
 end
@@ -199,11 +317,11 @@ function multi_free_energy(v_params, w_params, T, ϵ_optic, m_eff, volume, freqs
     # total dielectric contribution from all phonon branches (used as a normalisation)
     ϵ_tot = ϵ_total(freqs_and_ir_activity, volume)
 
-    # Generalisation of B i.e. Equation 62c in Hellwarth.
+    # Generalisation of B c.f. Equation 62c in Hellwarth.
     B_j_integrand(τ, β, v, w) = cosh(β / 2 - abs(τ)) / (sinh(β / 2) * sqrt(D_j(abs(τ), β, v, w)))
     B_j(β, α, v, w) = α / √π * quadgk(τ -> B_j_integrand(τ, β, v, w), 0.0, β / 2)[1]
 
-    # Generalisation of C i.e. Equation 62e in Hellwarth.
+    # Generalisation of C c.f. Equation 62e in Hellwarth.
     function C_j(β, v, w)
         s = 0.0
         for i in 1:length(v)
@@ -214,7 +332,7 @@ function multi_free_energy(v_params, w_params, T, ϵ_optic, m_eff, volume, freqs
         3 * s / num_of_branches
     end
 
-    # Generalisation of A i.e. Equation 62b in Hellwarth.
+    # Generalisation of A c.f. Equation 62b in Hellwarth.
     function A_j(β, v, w)
         s = -log(2π * β) / 2
         for i in 1:length(v)
@@ -297,11 +415,8 @@ end
 function multi_memory_function(ν, β::Array, α::Array, v, w, ω::Array, m_eff)
 
     Calculate polaron complex memory function inclusive of multiple phonon branches j, each with angular frequency ω[j] (rad THz).
-    α is an array of decomposed Frohlich alphas, one for each phonon frequency ω[j].
-    β is an array of reduced thermodynamic betas, one for each phonon frequency ω[j].
-    ν is the frequency (THz) of applied electric field.
-    v and w are variational parameters that minimise the polaron free energy for the system.
-    m_eff is the conduction band mass of the particle (typically electron / hole)
+    α is an array of decomposed Frohlich alphas, one for each phonon frequency ω[j]. β is an array of reduced thermodynamic betas, one for each phonon frequency ω[j]. ν is the frequency (THz) of applied electric field.
+    v and w are variational parameters that minimise the polaron free energy for the system. m_eff is the conduction band mass of the particle (typically electron / hole)
 
 Multiple phonon complex memory function.
 """
@@ -328,11 +443,8 @@ end
 function multi_conductivity(ν, β::Array, α::Array, v, w, ω::Array, m_eff)
 
     Calculate polaron complex conductivity inclusive of multiple phonon branches j, each with angular frequency ω[j] (rad THz).
-    α is an array of decomposed Frohlich alphas, one for each phonon frequency ω[j].
-    β is an array of reduced thermodynamic betas, one for each phonon frequency ω[j].
-    ν is the frequency (THz) of applied electric field.
-    v and w are variational parameters that minimise the polaron free energy for the system.
-    m_eff is the conduction band mass of the particle (typically electron / hole)
+    α is an array of decomposed Frohlich alphas, one for each phonon frequency ω[j]. β is an array of reduced thermodynamic betas, one for each phonon frequency ω[j]. ν is the frequency (THz) of applied electric field.
+    v and w are variational parameters that minimise the polaron free energy for the system. m_eff is the conduction band mass of the particle (typically electron / hole)
 
 Multiple phonon complex memory function.
 """
