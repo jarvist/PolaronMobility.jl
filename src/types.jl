d074cf1a02653e1a5c80274f54ef6af159650ee6
Bradley Martin <bam116@ic.ac.uk>
2021-12-14 17:56:35 +0000

    Update types.jl
    Added new NewPolaron type to contain output for the new multiple phonon code.

--- src/types.jl
+++ src/types.jl
@@ -6,6 +6,7 @@ const eV = const q = const ElectronVolt = 1.602176487e-19;                      
 const me=MassElectron = 9.10938188e-31;                          # kg
 const Boltzmann = const kB =  1.3806504e-23;                  # kg m2 / K s2
 const ε_0 = 8.854E-12 #Units: C2N−1m−2, permittivity of free space
+const c = 3e8
 
 
 # Structure to store data of polaron solution + other parameters, for each temperature
@@ -35,3 +36,23 @@ struct Polaron
 end
 Polaron()=Polaron([],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]) # structure initialisation
 
+struct NewPolaron
+    α      # Frohlich alpha (unitless)
+    T      # Temperature (K)
+    β      # Reduced Thermodynamic beta (unitless)
+    v      # Variational parameter (s^-1)
+    w      # Variational parameter (s^-1)
+    κ      # Fictitious spring constant (multiples of m_e) (kg / s^2)
+    M      # Fictitious particle (multiples of m_e) (kg)
+    F      # Free energy (meV)
+    Ω      # Electric field frequencies (multiples of phonon frequency ω) (s^-1)
+    Z      # Complex impedence (cm^2/Vs)
+    σ      # Complex conductivity (cm^-1)
+    μ      # Mobility (cm^2/Vs)
+end
+
+# Broadcast Polaron data.
+function Base.show(io::IO, x::NewPolaron)
+    flush(stdout)
+    print(io, "---------------------- \n Polaron Information: \n----------------------\n", "α = ", round(x.α, digits = 3), "\nT = ", round.(x.T, digits = 3), " K \nβ = ", round.(x.β, digits = 3), "\nv = ", round.(x.v, digits = 3), " s^-1\nw = ", round.(x.w, digits = 3), " s^-1\nκ = ", round.(x.κ, digits = 3), " kg/s^2\nM = ", round.(x.M, digits = 3), " kg\nF = ", round.(x.F, digits = 3), " meV\nΩ = ", round.(Float64.(x.Ω), digits = 3),  " s^-1\nZ = ", x.Z .|> y -> round.(ComplexF64.(y), digits = 3), " cm^2/Vs\nσ = ", x.σ .|> y -> round.(ComplexF64.(y), digits = 3), " cm^-1\nμ = ", round.(Float64.(x.μ), digits = 3))
+end
