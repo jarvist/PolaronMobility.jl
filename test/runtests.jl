afdba25855c89225139ccb3d3d9a98c818b583a3
Bradley Martin <bam116@ic.ac.uk>
2022-01-18 18:41:02 +0000

    Update runtests.jl
    Included new MultiplePhonon.jl tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,6 +12,7 @@ include("FeynmanAthermal.jl") # Athermal Feynman tests
 include("HellwarthEffectiveFrequency.jl") # Test Hellwarth et al. 1999 PRB 'B' multiple branch reduction scheme
 include("FrostPolaronMobility2017.jl") # Reproduce values published in Frost 2017 PRB 
 include("MultipleBranches.jl") # Test explicit Oct 2019:-> explicit phonon branches
+include("MultiplePhonons.jl") # Test 2021 work on multiple phonon modes
 end
 
 println("\nThat's me! If I finished without interupting, all tests have passed.")
