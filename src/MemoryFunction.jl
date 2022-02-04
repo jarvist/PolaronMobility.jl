c5fdfd6e7146cb9e02ee9ecea4eceac7f988513d
Bradley Martin <bam116@ic.ac.uk>
2022-02-04 18:41:19 +0000

    Removed commented code and @tullio macros (reserved for higher-level code)

--- src/MemoryFunction.jl
+++ src/MemoryFunction.jl
@@ -5,103 +5,6 @@
 # [3] F. Peeters and J. Devreese, “Theory of polaron mobility,” inSolid State Physics,pp. 81–133, Elsevier, 1984.
 
 """
-polaron_memory_function_thermal(Ω::Float64, β::Float64, α::Float64, v::Float64, w::Float64; rtol = 1e-3)
-
-    Calculates the memory function χ(Ω) of the polaron at finite temperatures (equation (35) in FHIP 1962 [1]) for a given frequency Ω. β is the thermodynamic beta. v and w are the variational polaron parameters that minimise the free energy, for the supplied α Frohlich coupling. rtol specifies the relative error tolerance for the QuadGK integral and corresponds to the error of the entire function. 
-
-    Finite temperature and finite frequency memory function. Does not explicitly include the limits to zero frequency Ω → 0 or zero temperature β → ∞. Although, those limits can be approximated by taking Ω ⪅ O(1e-3) or β ⪆ O(100), explict and exact functions are provided.
-"""
-# function polaron_memory_function_thermal(Ω, β, α, v, w; ω = 1.0, rtol = 1e-3)
-
-#     v = v[1]
-#     w = w[1]
-
-#     # FHIP1962, page 1011, eqn (47c).
-#     R = (v^2 - w^2) / (w^2 * v)  
-
-#     # FHIP1962, page 1009, eqn (35c).
-#     D(x) = w^2 / v^2 * (R * (1 - cos(v * x)) * coth(β[1] * v / 2) + x^2 / β[1] - 1im * (R * sin(v * x) + x))
-
-#     # FHIP1962, page 1009, eqn (36).
-#     S(x) = 2 * α / (3 * √π * 2π) * (exp(1im * x) + 2 * cos(x) / (exp(β[1]) - 1)) / (D(x))^(3 / 2)
-
-#     # FHIP1962, page 1009, eqn (35a).
-#     integrand(x) = (1 - exp(1im * Ω * 2π * x / ω)) * imag(S(x)) / Ω
-
-#     # Integrate using adapative quadrature algorithm with relative error tolerance rtol.
-#     integral, error = ω^2 .* quadgk(x -> integrand(x), 0.0, Inf64, rtol = rtol)
-
-#     return integral
-# end
-
-"""
-polaron_memory_function_athermal(Ω::Float64, α::Float64, v::Float64, w::Float64)
-
-    Calculate the memory function χ(Ω) of the polaron at zero temperatures (equations (12, 13) in DSG 1972 [2]) for a given frequency Ω. v and w are the variational polaron parameters that minimise the free energy, for the supplied α Frohlich coupling. rtol specifies the relative error tolerance for the QuadGK integral and corresponds to the error of the entire function. 
-        
-    Zero temperature and finite frequency memory function. Explicitly includes the limit to zero temperature β → ∞.
-"""
-# function polaron_memory_function_athermal(Ω, α, v, w; ω = 1.0, rtol = 1e-3)
-
-#     v = v[1]
-#     w = w[1]
-
-# 	# FHIP1962, page 1011, eqn (47c).
-# 	R = (v^2 - w^2) / (w^2 * v)
-
-# 	# FHIP1962, page 1009, eqn (35c) taken to athermal limit.
-# 	D(x) = w^2 / v^2 * (R * (1 - exp(im * v * x)) - 1im * x)
-
-# 	# FHIP1962, page 1009, eqn (36) taken to athermal limit.
-# 	S(x) = 2 * α / (3 * √π * 2π) * (exp(1im * x)) / (D(x))^(3 / 2)
-
-# 	# FHIP1962, page 1009, eqn (35a) taken to athermal limit.
-# 	integrand(x) = (1 - exp(1im * Ω * 2π * x / ω)) * imag(S(x)) / Ω
-
-#     # Integrand is an exponentially decaying oscillation. Provide an upper cut-off to the integral to ensure convergence. Using Inf results in a NaN. This mimics having a finite sum in the correspond hypergeometric function expansion for the integral.
-# 	integral, error = ω^2 .* quadgk(x -> integrand(x), 0.0, 1e205, rtol = rtol) # 
-
-#     # When the frequency Ω ≤ 0 the imaginary part of the memory function is zero. This corresponds to no phonon emission below the phonon frequency of the longitudinal optical mode ω_LO.
-# 	# if Ω <= 1
-# 	# 	return real(integral) + im * 0.0
-# 	# else
-# 	# 	return integral
-# 	# end
-
-#     return integral
-# end
-
-"""
-polaron_memory_function_dc(β::Float64, α::Float64, v::Float64, w::Float64)
-
-    Calculate the memory function lim(Ω → 0){χ(Ω) / Ω} of the polaron at finite temperatures (where χ(Ω) is from equation (35) in FHIP 1962 [1]) at zero frequency. v and w are the variational polaron parameters that minimise the free energy, for the supplied α Frohlich coupling. rtol specifies the relative error tolerance for the QuadGK integral and corresponds to the error of the entire function. 
-        
-    Zero frequency memory function. Explicitly includes the limit to zero frequency Ω → 0.
-"""
-# function polaron_memory_function_dc(β, α, v, w; ω = 1.0, rtol = 1e-3)
-
-#     v = v[1]
-#     w = w[1]
-
-#     # FHIP1962, page 1011, eqn (47c).
-#     R = (v^2 - w^2) / (w^2 * v)
-
-#     # FHIP1962, page 1009, eqn (35c).
-#     D(x) = w^2 / v^2 * (R * (1 - cos(v * x)) * coth(β[1] * v / 2) + x^2 / β[1] - 1im * (R * sin(v * x) + x))
-
-#     # FHIP1962, page 1009, eqn (36).
-#     S(x) = 2 * α / (3 * √π) * (exp(1im * x) + 2 * cos(x) / (exp(β[1]) - 1)) / (D(x))^(3 / 2)
-
-#     # FHIP1962, page 1009, eqn (35a) taken to dc zero frequency limit.
-#     integrand(x) = -im * x * imag(S(x))
-
-#     # Integrate using adapative quadrature algorithm with relative error tolerance rtol.
-#     integral, error = ω .* quadgk(x -> integrand(x), 0.0, Inf64, rtol = rtol)
-
-#     return integral
-# end
-
-"""
 polaron_memory_function(Ω::Float64, β::Float64, α::Float64, v::Float64, w::Float64)
 
     Calculate the memory function χ(Ω) of the polaron at finite temperatures (equation (35) in FHIP 1962 [1]) for a given frequency Ω. This function includes the zero-temperature and DC limits. β is the thermodynamic beta. v and w are the variational polaron parameters that minimise the free energy, for the supplied α Frohlich coupling. rtol specifies the relative error tolerance for the QuadGK integral and corresponds to the error of the entire function. 
@@ -160,9 +63,9 @@ function polaron_memory_function_thermal(Ω, β, α, v, w; ω = 1.0, rtol = 1e-3
     # FHIP1962, page 1009, eqn (35a).
     integrand(t, β, Ω) = (1 - exp(1im * Ω * 2π * t)) * imag(S(t, β))
 
-    @tullio integral[j] := quadgk(t -> integrand(t, β[j], Ω / ω[j]), 0.0, Inf64, rtol = rtol)
+    integral = map((x, y) -> quadgk(t -> integrand(t, x, Ω / y), 0.0, Inf64, rtol = rtol)[1], β, ω)
 
-    @tullio (+) memory := 2 * α[j] * ω[j]^2 * integral[j][1] / (3 * √π * Ω * 2π)
+     memory = sum(2 .* α .* ω .^2 .* integral ./ (3 * √π * Ω * 2π))
 
     return memory
 end
@@ -177,9 +80,9 @@ function polaron_memory_function_athermal(Ω, α, v, w; ω = 1.0, rtol = 1e-3)
 
     ω = length(ω) == 1 ? [ω] : ω
 
-    @tullio integral[j] := Ω < minimum(ω) / 2π ? (0.0 + im * 0.0, 0.0) : quadgk(t -> integrand(t, Ω / ω[j]), 0.0, 1 / rtol, rtol = rtol)
+    integral = map(x -> Ω < minimum(x) / 2π ? (0.0 + im * 0.0) : quadgk(t -> integrand(t, Ω / x), 0.0, 1 / rtol, rtol = rtol)[1], ω)
 
-    @tullio (+) memory := 2 * α[j] * ω[j]^2 * integral[j][1] / (3 * √π * Ω * 2π)
+    memory = sum(2 .* α .* ω .^2 .* integral ./ (3 * √π * Ω * 2π))
 
     return memory
 end
@@ -192,9 +95,9 @@ function polaron_memory_function_dc(β, α, v, w; ω = 1.0, rtol = 1e-3)
     # FHIP1962, page 1009, eqn (35a).
     integrand(t, β) = -im * t * imag(S(t, β))
 
-    @tullio integral[j] := quadgk(t -> integrand(t, β[j]), 0.0, 1 / rtol, rtol = rtol)
+    integral = map(x -> quadgk(t -> integrand(t, x), 0.0, 1 / rtol, rtol = rtol)[1], β)
 
-    @tullio (+) memory := 2 * α[j] * ω[j] * integral[j][1] / (3 * √π)
+    memory = sum(2 .* α .* ω .* integral ./ (3 * √π))
 
     return memory
 end
