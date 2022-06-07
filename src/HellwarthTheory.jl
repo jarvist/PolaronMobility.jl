4d735eb6be7a9e6b1fe05be37735f23dfb1125fc
Bradley Martin <bam116@ic.ac.uk>
2022-06-07 14:34:44 +0100

    Update docstrings

--- src/HellwarthTheory.jl
+++ src/HellwarthTheory.jl
@@ -2,26 +2,29 @@
 
 #####
 """
-	HellwarthBScheme(LO)
-    LO an array assumed to be of [freq ; absolute ir activity ]
+    HellwarthBScheme(LO)
 
-    Multiple phonon mode reduction to a single effective frequency.
-    Hellwarth et al. 1999 PRB, 'B scheme'; the athermal method.  Averaging procedure is
-    constructed by considering the average effect of the action of multiple branches.
+LO an array assumed to be of [freq ; absolute ir activity ]
 
-	Follows Eqn (58) in this paper, assuming typo on LHS, should actually be W_e.
+Multiple phonon mode reduction to a single effective frequency.
+Hellwarth et al. 1999 PRB, 'B scheme'; the athermal method.  Averaging procedure is
+constructed by considering the average effect of the action of multiple branches.
+
+Follows Eqn. (58) in this paper, assuming typo on LHS, should actually be W_e.
+
+See Hellwarth et a. 1999: https://doi.org/10.1103/PhysRevB.60.299.
 """
 function HellwarthBScheme(LO)
     println("Hellwarth B Scheme... (athermal)")
-    H58 = sum( (LO[:,2].^2)./ LO[:,1].^2 )
-    println("Hellwarth (58) summation: ",H58)
+    H58 = sum((LO[:, 2].^2) ./ LO[:, 1].^2)
+    println("Hellwarth (58) summation: ", H58)
 
-    H59 = sum( LO[:,2].^2 ) # sum of total ir activity squarred
+    H59 = sum(LO[:, 2].^2) # sum of total ir activity squarred
     println("Hellwarth (59) summation (total ir activity ^2): ", H59)
     println("Hellwarth (59) W_e (total ir activity ): ", sqrt(H59))
 
     omega = sqrt(H59 / H58)
-    println("Hellwarth (61) Omega (freq): ",omega)
+    println("Hellwarth (61) Omega (freq): ", omega)
 
 	return(omega)
 end
@@ -29,14 +32,16 @@ end
 # More complex scheme, involving thermodynamic Beta
 # Hellwarth(50), RHS
 """
-HellwarthAScheme(phonon_modes; T=295, convergence=1e-6)
+    HellwarthAScheme(phonon_modes; T = 295, convergence = 1e-6)
 
-    Multiple phonon mode reduction to a single effective frequency.
-	Temperature dependent, defaults to T = 295 K.
+Multiple phonon mode reduction to a single effective frequency.
+Temperature dependent, defaults to T = 295 K.
 
-    Solved iteratively by bisection until Δfreq<convergence.
+Solved iteratively by bisection until Δfreq < convergence.
 
-    Follows Hellwarth et al. 1999 PRB 'A' scheme, Eqn 50 RHS.
+Follows Hellwarth et al. 1999 PRB 'A' scheme, Eqn. (50) RHS.
+
+See Hellwarth et a. 1999: https://doi.org/10.1103/PhysRevB.60.299.
 """
 function HellwarthAScheme(phonon_modes; T = 295, convergence=1e-6)
 	   
