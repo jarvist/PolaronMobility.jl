f8365ccce49df6ba1c813444c080f54e50078480
Bradley A. Martin <bam116@ic.ac.uk>
2023-01-24 20:00:40 +0000

    Update main PolaronMobility.jl file to account for new files and function rearrangements.

--- src/PolaronMobility.jl
+++ src/PolaronMobility.jl
@@ -7,16 +7,14 @@ PolaronMobility.jl - https://github.com/jarvist/PolaronMobility.jl
 """
 module PolaronMobility
 
-export Polaron, NewPolaron, polaron, athermal_polaron, dc_polaron # Type to hold the data
-export Material, material
-export frohlichalpha, feynmanvw, F, polaronmobility, savepolaron
-export HellwarthBScheme, HellwarthAScheme
-export polaron_memory_function  # Polaron memory functions
-export optical_absorption       # Polaron optical absorption
-export ϵ_ionic_mode, multi_frohlichalpha, extended_feynmanvw, multi_F, polaron_mobility, polaron_complex_impedence, polaron_complex_conductivity
-export frohlichPartial, IRtoDielectric, IRtoalpha, DielectricFromIRmode
-export Hellwarth1999mobilityRHS, Hellwarth_mobility
-export make_polaron, save_polaron, load_polaron, combine_polarons
+export Polaron, polaron                           # Type to hold the polaron data.
+export Material, material                         # Type to hold material specific data.
+export frohlichalpha, ϵ_ionic_mode, F, feynmanvw  # Frohlich alpha, energy and minimisation functions.              
+export HellwarthBScheme, HellwarthAScheme         # Hellwarth effective mode schemes.
+export polaron_memory_function                    # Polaron memory functions
+export polaron_complex_impedence, polaron_complex_conductivity, optical_absorption        # Response functions.
+export polaron_mobility, Hellwarth_mobility, Kadanoff_mobility_lowT, FHIP_mobility_lowT   # Mobility functions.
+export save_polaron, load_polaron # Functions to save and load Polaron types to/from .jld format.
 export reduce_array
 
 
@@ -32,13 +30,30 @@ import QuadGK.quadgk
 # Using the powerful Julia Optim package to optimise the variational parameters
 using Optim
 
-include("Types.jl")            # Polaron types
-include("FeynmanTheory.jl")    # Actions + variational functions
-include("HellwarthTheory.jl")  # multimode -> equivalent mode.
-include("MobilityTheories.jl") # Main polaronmobility function
-include("MemoryFunction.jl")   # Memory function X calculation.
-include("Susceptibility.jl")   # ImX calculation
-include("OedipusRex.jl")       # Optical Absorption
+include("FeynmanTheory.jl")     # Actions + variational functions.
+include("HellwarthTheory.jl")   # multimode -> equivalent mode.
+include("MemoryFunction.jl")    # Memory function X calculation.
+include("ResponseFunctions.jl") # Linear reponse functions for polaron.
+include("Material.jl")          # Material type and constructors.
+include("Polaron.jl")           # Polaron type and constructors.
+
+# Physical Constants
+"Planck's constant, (kgm²s⁻¹)."
+const hbar = const ħ = 1.054571817e-34
+"Electron charge, (kgm²s⁻²)."
+const eV = const q = const ElectronVolt = 1.602176634e-19
+"Electron mass, (kg)."
+const me = MassElectron = 9.1093837015e-31
+"Boltzmann's constant, (kgm²K⁻¹)."
+const Boltzmann = const kB = 1.380649e-23
+"Permittivity of free space, (C²N⁻¹m⁻²)."
+const ε_0 = ϵ_0 = 8.85418682e-12
+"Speed of light, (ms⁻¹)."
+const c = 299792458
+"Atomic mass unit, (kg)"
+const amu = 1.660_539_066_60e-27
+
+reduce_array(a) = length(a) == 1 ? only(a) : dropdims(a, dims=tuple(findall(size(a) .== 1)...))
 
 end # module
 
